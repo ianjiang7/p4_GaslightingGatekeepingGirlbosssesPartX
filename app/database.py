0e5e4c4a410f95d2ac850727e1a5b99d646dec61
Gitae Park <78169596+gpark9@users.noreply.github.com>
2023-05-08 12:59:28 -0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,9 @@
+import sqlite3
 
+DB_FILE = "tables.db"
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
+c = db.cursor()
+
+command = "create table IF NOT EXISTS login(user TEXT, password TEXT)"
+c.execute(command)
+db.commit()

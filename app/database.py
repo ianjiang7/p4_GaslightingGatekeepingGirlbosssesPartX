9923eaacf3366adb34559374b0095a14c7aa65a6
Gitae Park <78169596+gpark9@users.noreply.github.com>
2023-05-09 10:27:44 -0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,6 +4,10 @@ DB_FILE = "tables.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False)
 c = db.cursor()
 
-command = "create table IF NOT EXISTS login(user TEXT, password TEXT)"
+command = "create table IF NOT EXISTS login(user TEXT, password TEXT, stock_id INTEGER);"
+c.execute(command)
+db.commit()
+
+command = "create table IF NOT EXISTS predictions(id INTEGER PRIMARY KEY, name TEXT, price REAL);"
 c.execute(command)
 db.commit()

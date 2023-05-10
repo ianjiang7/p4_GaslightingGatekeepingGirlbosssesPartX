c748a14b74720f42ee39982792bd995b983a1923
Aden Garbutt <agarbutt268@gmail.com>
2023-05-10 13:49:40 -0400

    updates

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,12 +4,12 @@ var test = function() {
 
 var body = document.getElementsByTagName('body')[0];
 console.log(body);
-var lightModeButton = document.getElementById('light-mode');
+var lightModeButton = document.getElementById('lightMode');
 var lightMode = function() {
     body.setAttribute('data-bs-theme', 'light');
 };
 
-var darkModeButton = document.getElementById('dark-mode');
+var darkModeButton = document.getElementById('darkMode');
 var darkMode = function() {
     body.setAttribute('data-bs-theme', 'dark');
 };

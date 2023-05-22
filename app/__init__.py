2906dbd8deff1597ee5ebee0f8cdb45d8df2e59b
Gitae Park <78169596+gpark9@users.noreply.github.com>
2023-05-22 07:48:18 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ DB_FILE = "tables.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False)
 c = db.cursor()
 
-command = "create table IF NOT EXISTS login(user TEXT, password TEXT, stock_id INTEGER);"
+command = "create table IF NOT EXISTS login(user TEXT, password TEXT);"
 c.execute(command)
 db.commit()
 
